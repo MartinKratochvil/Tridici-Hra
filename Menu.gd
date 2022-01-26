365c35468bbbe649f43bd98ef585c98807990e76
Nitram293 <kratochvilm.04@spst.eu>
2022-01-26 18:54:22 +0100

    upload #3

--- Menu.gd
+++ Menu.gd
@@ -6,5 +6,4 @@ func _ready():
 func _on_Level1_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed:
-			print(Score.score)
 			get_tree().change_scene("res://Minigame1.tscn")
